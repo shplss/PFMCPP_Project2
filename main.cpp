d748fc46e461672ea69b65b64e15d267c209a9dc
shplss <vasco.audio@gmail.com>
2020-06-04 09:14:00 +0000

    primitives added, 3 variable creations for each type and 10 functions created and called.

--- main.cpp
+++ main.cpp
@@ -16,13 +16,13 @@ video: Chapter 2 - Part 3
  
  1) Write down the names of all of the primitives available in C++ (excluding wchar_t) here: 
  
- 
- 
- 
- 
- 
- 
- 
+ int
+ float
+ double
+ bool
+ char
+ unsigned int
+ void
  
  
  
@@ -57,9 +57,55 @@ void variableDeclarations()
     //example:
     int number = 2; //declaration of a primitive named 'number' with an initial value of '2'
     
-    
+    int recordingChannels = 16;
+    int userAge = 18;
+    int midiNote = 60;
+
+    float chVolume = 1.0f;
+    float reverbAmount = 0.5f;
+    float licensePrice = 19.99f;
+
+    double pi = 3.14159265359;
+    double whLatitude = 38.8977;
+    double referenceNumber =344.55555555;
+
+    bool includeLowerOctave = false;
+    bool toOutput = true;
+    bool isPlaying = false;
+
+    char quitApp = 'n';
+    char gender = 'u';
+    char nameInitial = 's';
+
+    unsigned int progress = 0;
+    unsigned int distance = 100;
+    unsigned int productAmount = 10;
     
     ignoreUnused(number); //passing each variable declared to the ignoreUnused() function
+
+    ignoreUnused(recordingChannels);
+    ignoreUnused(userAge);
+    ignoreUnused(midiNote);
+
+    ignoreUnused(chVolume);
+    ignoreUnused(reverbAmount);
+    ignoreUnused(licensePrice);
+
+    ignoreUnused(pi);
+    ignoreUnused(whLatitude);
+    ignoreUnused(referenceNumber);
+
+    ignoreUnused(includeLowerOctave);
+    ignoreUnused(toOutput);
+    ignoreUnused(isPlaying);
+
+    ignoreUnused(quitApp);
+    ignoreUnused(gender);
+    ignoreUnused(nameInitial);
+
+    ignoreUnused(progress);
+    ignoreUnused(distance);
+    ignoreUnused(productAmount);
 }
 /*
  10 functions
@@ -74,43 +120,87 @@ bool rentACar(int rentalDuration, int carType = 0)  //function declaration with 
 /*
  1)
  */
-
+void createMixer(int nrOfChannels = 16, char mixerType = 'i')
+{
+    ignoreUnused(nrOfChannels, mixerType);
+}
 /*
  2)
  */
+float getCurrentTemperature(unsigned int countryCode, char tempUnit = 'c') 
+{
+    ignoreUnused(countryCode,tempUnit);
 
+    return {};
+}
 /*
  3)
  */
-
+void spawnPlayer(bool hasWeapons, bool hasPotions, unsigned int progressLocation = 0)
+{
+    ignoreUnused(hasWeapons, hasPotions, progressLocation);
+}
 /*
  4)
  */
+bool playMidiNote(int midiNote, int midiNoteVel, int midiChannel = 1, int midiProgramGM = 0)
+{
+    ignoreUnused(midiNote, midiNoteVel, midiChannel, midiProgramGM);
 
+    return {};
+}
 /*
  5)
  */
+float sumTwoNumbers(float number1, float number2)
+{
+    ignoreUnused(number1, number2);
 
+    return {};
+}
 /*
  6)
  */
+float priceWithTax(float productPrice, float VAT = 21.0f) 
+{
+    ignoreUnused(productPrice, VAT);
 
+    return {};
+}
 /*
  7)
  */
+int freq2midi(float freqHz) 
+{
+    ignoreUnused(freqHz);
 
+    return {};
+}
 /*
  8)
  */
-
+void insertCompressor(int compTypeCode, int channelNr, bool preEQ = true)
+{
+    ignoreUnused(compTypeCode, channelNr, preEQ);
+}
 /*
  9)
  */
+bool resetChannelFX(int channelNr)
+{
+    ignoreUnused(channelNr);
 
+    return {};
+}
 /*
  10)
  */
+float linear2dB(float linearVolume)
+{
+    ignoreUnused(linearVolume);
 
+    return {};
+}
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
@@ -131,27 +221,34 @@ int main()
     auto carRented = rentACar(6, 2); 
     
     //1)
-    
+    createMixer();
     //2)
-    
+    auto temp = getCurrentTemperature(66, 'f');
     //3)
-    
+    spawnPlayer(true, false);
     //4)
-    
+    auto playSuccess = playMidiNote(60, 100, 1, 50);
     //5)
-    
+    auto sum = sumTwoNumbers(2.5f, 8.9f);
     //6)
-    
+    auto retailPrice = priceWithTax(15.8f);
     //7)
-    
+    auto midiNote = freq2midi(340);
     //8)
-    
+    insertCompressor(5, 6, false);
     //9)
-    
+    auto resetSuccess = resetChannelFX(13);
     //10)
-    
+    auto dBLevel = linear2dB(0.35f);
     
     ignoreUnused(carRented);
+    ignoreUnused(temp);
+    ignoreUnused(playSuccess);
+    ignoreUnused(sum);
+    ignoreUnused(retailPrice);
+    ignoreUnused(midiNote);
+    ignoreUnused(resetSuccess);
+    ignoreUnused(dBLevel);
     std::cout << "good to go!" << std::endl;
     return 0;    
 }
